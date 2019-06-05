42457c5bd953148cf9bc01cd7ac0d4bff2700f06
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-06-05 16:11:00 -0700

    added models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Dog
+from .models import Barge, Customer, CustomerContact, CustomerStanding, Equipment, EquipmentTypeDetails, PaymentMethod, ServiceCenter, Shipment, Stop, Tug, Voyage
 
 # Register your models here.
-admin.site.register(Dog)
+admin.site.register(Barge)
+admin.site.register(Customer)
+admin.site.register(CustomerContact)
+admin.site.register(CustomerStanding)
+admin.site.register(Equipment)
+admin.site.register(EquipmentTypeDetails)
+admin.site.register(PaymentMethod)
+admin.site.register(ServiceCenter)
+admin.site.register(Shipment)
+admin.site.register(Stop)
+admin.site.register(Tug)
+admin.site.register(Voyage)
--- main_app/models.py
+++ main_app/models.py
@@ -2,19 +2,82 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class DogToy(models.Model):
-    name = models.CharField(max_length=100)
-    color = models.CharField(max_length=50)
-    
-    def __str__(self):
-        return self.name
+class Barge(models.Model):
+    name = models.CharField(max_length=60)
 
-class Dog(models.Model):
-    name = models.CharField(max_length=100)
-    desc = models.TextField(max_length=255)
-    age = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    dog_toys = models.ManyToManyField(DogToy)
+class Customer(models.Model):
+    name = models.CharField(max_length=60)
+    credit_limit = models.IntegerField(max_length=11)
+    standing = models.ForeignKey(CustomerStanding)
 
     def __str__(self):
         return self.name
+
+class CustomerContact(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    phone = models.IntegerField(max_length=10)
+    address_line_1 = models.CharField(max_length=100)
+    address_line_2 = models.CharField(max_length=50)
+    address_city = models.CharField(max_length=50)
+    address_state = models.CharField(max_length=50)
+    address_zip = models.IntegerField(max_length=9)
+
+class CustomerStanding(models.Model):
+    code = models.IntegerField(max_length=3)
+    desc = models.CharField(max_length=50)
+
+class Equipment(models.Model):
+    no = models.CharField(max_length=10)
+    status = models.CharField(max_length=10)
+    type = models.ForeignKey(EquipmentTypeDetails)
+
+class EquipmentTypeDetails(models.Model):
+    name = models.CharField(max_length=20)
+
+class PaymentMethod(models.Model):
+    name = models.CharField(max_length=60)
+
+class PaymentTransaction(models.Model):
+    made_by = models.CharField(max_length=60)
+    method = models.ForeignKey(PaymentMethod)
+    note = models.CharField(max_length=255)
+    date_time = models.IntegerField(max_length=20)
+    origin = models.ForeignKey(ServiceCenter)
+    card_last_four = models.IntegerField(max_length=4)
+
+class ServiceCenter(models.Model):
+    name = models.CharField(max_length=40)
+    code = models.CharField(max_length=3)
+    lat = models.AutoField(max_length=11)
+    lng = models.AutoField(max_length=11)
+
+class Shipment(models.Model):
+    no = models.IntegerField(max_length=60)
+    origin = models.ForeignKey(ServiceCenter)
+    destination = models.ForeignKey(ServiceCenter)
+    shipper = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    consignee = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    billto = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    equipment = models.ForeignKey(Equipment)
+
+class Stop(models.Model):
+    voyage = models.ForeignKey(Voyage)
+    sequence = models.IntegerField(max_length=2)
+    service_center = models.ForeignKey(ServiceCenter)
+    arrival_expected = models.IntegerField(max_length=20)
+    arrival_actual = models.IntegerField(max_length=20)
+    departure_expected = models.IntegerField(max_length=20)
+    departure_actual = models.IntegerField(max_length=20)
+
+class Tug(models.Model):
+    name = models.CharField(max_length=40)
+    status = models.CharField(max_length=20)
+    last_lng = models.AutoField(max_length=11)
+    last_lat = models.AutoField(max_length=11)
+
+class Voyage(models.Model):
+    no = models.CharField(max_length=60)
+    tug = models.ForeignKey(Tug)
+    barge = models.ForeignKey(Barge)
+    current_sequence = models.IntegerField(max_length=2)
+    finished = models.BooleanField()
